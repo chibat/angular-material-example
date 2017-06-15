54ec116703216c96f8c6497442c418424f621219
chibat <tomofumi.chiba@gmail.com>
2017-06-16 00:04:34 +0900

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,32 @@ export class AppComponent {
   inSearch = false;
   cardSelectedFlag = false;
 
+  visibleMoveButton = false;
+
+  constructor() {
+    window.addEventListener('scroll', (e) => {
+      if (window.pageYOffset < 100) {
+          this.visibleMoveButton = false;
+      } else {
+          this.visibleMoveButton = true;
+      }
+      if (this.getScrollBottom() === 0) {
+        alert('bottom');
+      }
+    });
+  }
+
+  private getScrollBottom() {
+    var body = window.document.body;
+    var html = window.document.documentElement;
+    var scrollTop = body.scrollTop || html.scrollTop;
+    return html.scrollHeight - html.clientHeight - scrollTop;
+  }
+
+  scrollToTop() {
+    window.scrollTo(0, 0);
+  }
+
   search() {
     this.inSearch = true;
     setTimeout(() => {
